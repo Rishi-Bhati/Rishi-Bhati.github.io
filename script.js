cbcf0258b7ab8ea09cc77588fa10d7d5c00492b0
Rishi-Bhati <bhatirishi5@gmail.com>
2024-04-30 21:29:23 +0530

    Changed to currency converter

--- script.js
+++ script.js
@@ -1,60 +1,55 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const display = document.getElementById("display");
-    const keys = document.querySelectorAll(".keys button");
-    let count = 1;
+const BASE_URL = "https://2024-03-06.currency-api.pages.dev/v1/currencies";
 
-    keys.forEach(key => {
-        key.addEventListener("click", function() {
-            const keyValue = key.textContent;
-            
-            if (keyValue === "=") {
-                try {
-                    // display.value = eval(display.value);
-                    const result = new Function('return ' + display.value)();
-                    display.value = isNaN(result) ? 'Error' : result;
-                } catch (error) {
-                    display.value = "Error";
-                }
-            } else if (keyValue === "C") {
-                display.value = "";
-                count = 1;
-            } else if (keyValue === "( )") {
-                if (count == 1) {
-                    
-                    const expressionBeforeCaret = display.value.substring(0, display.selectionStart);
-                    const matches = expressionBeforeCaret.match(/[0-9.]+$/);
-                    if (matches) {
-                        const lastNumber = matches[0];
-                        display.value = expressionBeforeCaret.replace(/[0-9.]+$/, lastNumber + "*");
-                    }
-                    display.value += "(";
-                    count = 2;
-                } else if (count == 2) {
-                    const expressionBeforeCaret = display.value.substring(0, display.selectionStart);
-                    const matches = expressionBeforeCaret.match(/["("]+$/);
-                    if (matches) {
-                        const lastNumber = matches[0];
-                        display.value = expressionBeforeCaret.replace(/["("]+$/, lastNumber + "0");
-                    }
-                    display.value += ")";
-                    count = 1;
-                }
-            } else if (keyValue == "<=") {
-                const expressionBeforeCaret = display.value.substring(0, display.selectionStart-1);
-                display.value = expressionBeforeCaret;
-            }
-            
-            else if (keyValue === "%") {
-                 
-                const expressionBeforeCaret = display.value.substring(0, display.selectionStart);
-                
-                const matches = expressionBeforeCaret.match(/[0-9.]+$/);
-                if (matches) {
-                    const lastNumber = parseFloat(matches[0]);
-                    display.value = expressionBeforeCaret.replace(/[0-9.]+$/, lastNumber / 100);
-                } } else {
-                display.value += keyValue;
-            }
-        });
-    });
-});
+const dropdowns = document.querySelectorAll(".dropdown select");
+
+const btn = document.querySelector("form button");
+
+const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".to select");
+
+const msg = document.querySelector(".msg");
+
+for(let select of dropdowns) {
+    for (currCode in countryList) {
+        let newOption = document.createElement("option");
+        newOption.innerText = currCode;
+        newOption.value = currCode;
+        if(select.name === "from" && currCode === "USD") {
+            newOption.selected = "selected";
+        } else if (select.name === "to" && currCode === "INR"){
+            newOption.selected = "selected";
+        }
+        select.append(newOption);
+    }
+    select.addEventListener("change", (evt) => { 
+        updateFlag(evt.target);
+    })
+};
+
+const updateFlag = (element) => {
+    let currCode = element.value;
+    let countryCode = countryList[currCode];
+    let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+    let img = element.parentElement.querySelector("img");
+    img.src = newSrc;
+};
+
+btn.addEventListener("click", async (evt) => {
+    evt.preventDefault();
+    let amount = document.querySelector(".amount input");
+    let amtValue = amount.value;
+    if(amtValue === "" || amtValue <1 || typeof(amtValue) === 'string'){
+        amtValue = 1;
+        amount.value = 1
+    }
+
+    // console.log(fromCurr.value);
+    // console.log(toCurr.value);
+    const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}.json`;
+    let response = await fetch(URL);
+    let data = await response.json();
+    let dte = data.date
+    let rate = data[fromCurr.value.toLowerCase()][toCurr.value.toLowerCase()];
+    let finalAmt = amtValue * rate;
+    msg.innerText = `${amtValue} ${fromCurr.value} = ${finalAmt} ${toCurr.value} as of date: ${dte}`;
+})
